6281fc8c97a6019b589ffe997336f9015f01ffb8
mcappalleti <josselinemari@hotmail.es>
2019-08-16 17:01:33 -0500

    Agregando Arquitectura basica

---
+++ SoporteTecnico/SoporteTecnico.BL/ProductosBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoporteTecnico.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Mantenimiento Preventivo";
+            producto1.Precio = 300.00;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Taller de Reparaciones";
+            producto2.Precio = 150.00;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Instalaciones de Software";
+            producto3.Precio = 250.00;
+
+            var listadeProductos = new List<Producto>();
+
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+
+            return listadeProductos;
+        }
+    }
+}
--- SoporteTecnico/SoporteTecnico.Web/Controllers/ServiciosController.cs
+++ SoporteTecnico/SoporteTecnico.Web/Controllers/ServiciosController.cs
@@ -1,4 +1,4 @@
-﻿using SoporteTecnico.Web.Models;
+﻿using SoporteTecnico.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,24 +12,8 @@ namespace SoporteTecnico.Web.Controllers
         // GET: Servicios
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Mantenimiento Preventivo";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Taller de Reparaciones";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Instalaciones de Software";
-
-            var listadeProductos = new List<ProductoModel>();
-
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
